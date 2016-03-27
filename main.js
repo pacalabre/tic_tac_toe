f0a78e6a078419445465e5833d08efb67d0438a7
Paul <pacalabre@gmail.com>
2016-03-26 21:23:11 -0700

    added counter to toggle between turns

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 
+//function toggleTurn() {
+var counter = true;
+
+  function changeCounter() {
+    if (counter === true) {
+      counter = false;
+    } else if ( counter === false) {
+      counter = true;
+    }
+  }
+
+  //changeCounter();
+  //console.log(counter);
+//}
+
 document.getElementById('position1').addEventListener("click", function(){
   console.log("click");
+  //toggleTurn();
+  changeCounter();
+  console.log(counter);
   document.getElementById('posOneText').innerHTML="x";
 })
