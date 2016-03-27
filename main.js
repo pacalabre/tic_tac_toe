6237ed1ff239edffc7c7c58c92d278f96610efb1
Paul <pacalabre@gmail.com>
2016-03-27 14:35:15 -0700

    working on making JS dry

--- main.js
+++ main.js
@@ -12,101 +12,146 @@ var counter = true;
 
 
 //Position 1
-document.getElementById('position1').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
 
-  document.getElementById('posOneText').innerHTML="X";
-  } else {
-  document.getElementById('posOneText').innerHTML="O";
-  }
-  toggleTurn();
-})
+  document.getElementById('position1').addEventListener("click", function(){
+    if (counter === true) {
+    console.log(counter);
 
-//Position 2
-document.getElementById('position2').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posTwoText').innerHTML="X";
-  } else {
-  document.getElementById('posTwoText').innerHTML="O";
-  }
-  toggleTurn();
-})
+    document.getElementById('posOneText').innerHTML="X";
+    } else {
+    document.getElementById('posOneText').innerHTML="O";
+    }
+    toggleTurn();
+  })
 
-//Position 3
-document.getElementById('position3').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posThreeText').innerHTML="X";
-  } else {
-  document.getElementById('posThreeText').innerHTML="O";
-  }
-  toggleTurn();
-})
 
-//Position 4
-document.getElementById('position4').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posFourText').innerHTML="X";
-  } else {
-  document.getElementById('posFourText').innerHTML="O";
-  }
-  toggleTurn();
-})
+var buttons = [];
 
-//Position 5
-document.getElementById('position5').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posFiveText').innerHTML="X";
-  } else {
-  document.getElementById('posFiveText').innerHTML="O";
-  }
-  toggleTurn();
-})
+for(var i = 1; i <= 9; i++) {
+  buttons.push('position'+i);
 
-//Position 6
-document.getElementById('position6').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posSixText').innerHTML="X";
-  } else {
-  document.getElementById('posSixText').innerHTML="O";
-  }
-  toggleTurn();
-})
+  // document.getElementById(buttons[i]).addEventListener("click", function() {
+  // if (counter === true) {
+  //   console.log(counter);
 
-//Position 7
-document.getElementById('position7').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posSevenText').innerHTML="X";
-  } else {
-  document.getElementById('posSevenText').innerHTML="O";
-  }
-  toggleTurn();
-})
+  //   document.getElementById('pos'+i+'Text').innerHTML="X";
+  //   } else {
+  //   document.getElementById('pos'+i+'Text').innerHTML="O";
+  //   }
+  //   toggleTurn();
+  // })
+}
 
-//Position 8
-document.getElementById('position8').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posEightText').innerHTML="X";
-  } else {
-  document.getElementById('posEightText').innerHTML="O";
-  }
-  toggleTurn();
-})
+for (var i =0; i < buttons.length; i++) {
+  var gameButtons = buttons[i];
+  console.log(gameButtons);
+}
 
-//Position 9
-document.getElementById('position9').addEventListener("click", function(){
-  if (counter === true) {
-  console.log(counter);
-  document.getElementById('posNineText').innerHTML="X";
-  } else {
-  document.getElementById('posNineText').innerHTML="O";
-  }
-  toggleTurn();
-})
+
+
+
+
+// for (var i = 1; i <= 9; i++) {
+//   var placeX;
+//   var placeO;
+//   var element = 'pos'+i+'Text';
+//   document.getElementById('position'+i).addEventListener("click", function(e){
+//     if (counter === true) {
+//     console.log(element);
+//     placeX = e.target.element.innerHTML="X";
+//     } else {
+//     placeO = e.target.element.innerHTML="O";
+//     }
+//     toggleTurn();
+//   })
+//   console.log('element='+element);
+//   console.log('placeX = ' + placeX + ' placeY = '+placeO);
+// }
+
+// //Position 2
+// document.getElementById('position2').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posTwoText').innerHTML="X";
+//   } else {
+//   document.getElementById('posTwoText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 3
+// document.getElementById('position3').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posThreeText').innerHTML="X";
+//   } else {
+//   document.getElementById('posThreeText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 4
+// document.getElementById('position4').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posFourText').innerHTML="X";
+//   } else {
+//   document.getElementById('posFourText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 5
+// document.getElementById('position5').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posFiveText').innerHTML="X";
+//   } else {
+//   document.getElementById('posFiveText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 6
+// document.getElementById('position6').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posSixText').innerHTML="X";
+//   } else {
+//   document.getElementById('posSixText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 7
+// document.getElementById('position7').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posSevenText').innerHTML="X";
+//   } else {
+//   document.getElementById('posSevenText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 8
+// document.getElementById('position8').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posEightText').innerHTML="X";
+//   } else {
+//   document.getElementById('posEightText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
+
+// //Position 9
+// document.getElementById('position9').addEventListener("click", function(){
+//   if (counter === true) {
+//   console.log(counter);
+//   document.getElementById('posNineText').innerHTML="X";
+//   } else {
+//   document.getElementById('posNineText').innerHTML="O";
+//   }
+//   toggleTurn();
+// })
