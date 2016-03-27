668b6ba124323bbefd8365d06b451a57f8161d32
Paul <pacalabre@gmail.com>
2016-03-26 21:55:37 -0700

    added button x and o functionability to all buttons

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var counter = true;
   }
 
 
-
+//Position 1
 document.getElementById('position1').addEventListener("click", function(){
   if (counter === true) {
   console.log(counter);
@@ -23,12 +23,90 @@ document.getElementById('position1').addEventListener("click", function(){
   toggleTurn();
 })
 
+//Position 2
 document.getElementById('position2').addEventListener("click", function(){
   if (counter === true) {
   console.log(counter);
   document.getElementById('posTwoText').innerHTML="x";
   } else {
   document.getElementById('posTwoText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 3
+document.getElementById('position3').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posThreeText').innerHTML="x";
+  } else {
+  document.getElementById('posThreeText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 4
+document.getElementById('position4').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posFourText').innerHTML="x";
+  } else {
+  document.getElementById('posFourText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 5
+document.getElementById('position5').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posFiveText').innerHTML="x";
+  } else {
+  document.getElementById('posFiveText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 6
+document.getElementById('position6').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posSixText').innerHTML="x";
+  } else {
+  document.getElementById('posSixText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 7
+document.getElementById('position7').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posSevenText').innerHTML="x";
+  } else {
+  document.getElementById('posSevenText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 8
+document.getElementById('position8').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posEightText').innerHTML="x";
+  } else {
+  document.getElementById('posEightText').innerHTML="O";
+  }
+  toggleTurn();
+})
+
+//Position 9
+document.getElementById('position9').addEventListener("click", function(){
+  if (counter === true) {
+  console.log(counter);
+  document.getElementById('posNineText').innerHTML="x";
+  } else {
+  document.getElementById('posNineText').innerHTML="O";
   }
   toggleTurn();
 })
